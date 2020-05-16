0a5f41700f2656b6d3fcb20b61afd65f10c541f9
sketch-coding <celtonrd7942@gmail.com>
2020-05-16 14:12:52 +0900

    DivTag.vue파일 생성

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div-tag />
+  </div>
+</template>
+
+<script>
+import DivTag from './components/tag/DivTag.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'div-tag': DivTag
+  }
+}
+</script>
+
+<style>
+body {
+  margin-top: 0px;
+  padding: 0px;
+}
+</style>
