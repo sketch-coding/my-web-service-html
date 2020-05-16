f5e9e3d98096ded67506483d8961d9f34b7d22c3
sketch-coding <celtonrd7942@gmail.com>
2020-05-16 15:50:30 +0900

    TextTag.vue파일 생성 및 등록

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <div-tag />
+    <!-- <div-tag /> -->
+    <text-tag />
   </div>
 </template>
 
 <script>
 import DivTag from './components/tag/DivTag.vue'
+import TextTag from './components/tag/TextTag'
 
 export default {
   name: 'App',
   components: {
-    'div-tag': DivTag
+    'div-tag': DivTag,
+    'text-tag': TextTag
   }
 }
 </script>
---
+++ src/components/tag/TextTag.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Text Tag</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
