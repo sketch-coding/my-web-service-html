c2d1db32ff22bc35564c0fdc857707657013f9eb
sketch-coding <celtonrd7942@gmail.com>
2020-05-16 16:41:47 +0900

    heading 태그에 CSS 적용

--- src/components/tag/TextTag.vue
+++ src/components/tag/TextTag.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>Text Tag</div>
+    <div>
+        <h1>제목 h1 태그</h1>
+        <h2>제목 h2태그</h2>
+        <h3 class="green">제목 h3 태그</h3>
+        <h4 class="green">제목 h4 태그</h4>
+        <h5 class="green">제목 h5 태그</h5>
+        <h6 class="green">제목 h6 태그</h6>
+    </div>
 </template>
 
 <script>
@@ -9,5 +16,13 @@ export default {
 </script>
 
 <style scoped>
-
+    h1 {
+        color: red;
+    }
+    h2 {
+        color: blue;
+    }
+    .green {
+        color: green;
+    }
 </style>
